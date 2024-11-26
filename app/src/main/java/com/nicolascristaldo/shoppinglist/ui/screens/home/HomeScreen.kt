c441f5c3b3f6dbcc602236b66273b26c5dab8577
nicolas cristaldo <crist4545@gmail.com>
2024-11-26 15:00:21 -0300

    typography added

--- app/src/main/java/com/nicolascristaldo/shoppinglist/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/nicolascristaldo/shoppinglist/ui/screens/home/HomeScreen.kt
@@ -2,11 +2,15 @@ package com.nicolascristaldo.shoppinglist.ui.screens.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.nicolascristaldo.shoppinglist.R
 import com.nicolascristaldo.shoppinglist.ui.components.AppButton
@@ -30,11 +34,18 @@ fun HomeScreen(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
-        Text(text = stringResource(id = R.string.app_name))
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.displaySmall,
+            modifier = Modifier
+                .padding(bottom = dimensionResource(id = R.dimen.large_padding))
+        )
 
         AppButton(
             title = stringResource(id = R.string.add_product),
-            onClick = { navigateToAddProduct() }
+            onClick = { navigateToAddProduct() },
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.small_padding))
         )
 
         AppButton(
@@ -42,4 +53,10 @@ fun HomeScreen(
             onClick = { navigateToList() }
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPeview() {
+    HomeScreen(navigateToAddProduct = {  }, navigateToList = { })
 }
--- app/src/main/java/com/nicolascristaldo/shoppinglist/ui/theme/Type.kt
+++ app/src/main/java/com/nicolascristaldo/shoppinglist/ui/theme/Type.kt
@@ -2,12 +2,32 @@ package com.nicolascristaldo.shoppinglist.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.nicolascristaldo.shoppinglist.R
+
+val LexendExa = FontFamily(
+    Font(R.font.lexend_exa_extra_bold, FontWeight.ExtraBold)
+)
+
+val PlusJakartaSans = FontFamily(
+    Font(R.font.plus_jakarta_sans_light, FontWeight.Light),
+    Font(R.font.plus_jakarta_sans_regular, FontWeight.Normal),
+    Font(R.font.plus_jakarta_sans_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displaySmall = TextStyle(
+        fontFamily = LexendExa,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 38.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.5.sp
+    ),
+
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
